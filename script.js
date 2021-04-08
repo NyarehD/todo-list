58af52dea1248f9dc9b93ab49ae8cccac1d240d4
NyarehD <nyarehdavid56@gmail.com>
2021-04-08 14:39:57 +0630

    Todo tasks can be added to the list

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
-let inputText = document.getElementById('inputText');
+let inputText = document.getElementById('inputTask');
 const addButton = document.getElementById('addButton');
-let todoList = document.getElementById('todoList');
+const todoList = document.getElementById('todoList');
+let inputDescription = document.getElementById('inputDescription');
+let listId = 1;
 
-inputText.addEventListener("input", text => {
-    document.getElementById("pp").innerHTML = text.target.value;
-    console.log(text);
-})
+
+
+addButton.addEventListener("click", ()=>{
+    inputTextToList();    
+});
+
+// Adding tasks to todo list
+function inputTextToList(){
+    todoList.innerHTML += `<li id="listId${listId}">
+                                <div>
+                                    <h3>${inputText.value}</h3>
+                                    <p>${inputDescription.value}</p>
+                                </div>
+                                <div>
+                                    <button src="/assets/edit-regular.svg" onclick="editList('${listId}')">edit</button>
+                                    <button src="" onclick="deleteList('${listId})">Delete</button>
+                                </div>
+                            </li>`;
+    listId++;
+}
+// Edit tasks from todo list
+function editList(id){
+    let currentTask = document.getElementById(`listId${id}`);
+    currentTask.innerHTML = `<div>
+                                 <input placeholder="Edit your Task Name" value="$>
+                                 <textarea cols="30" row="5"></textarea>
+                                </div>   
+                            <div>
+                                <button>Done</button>
+                                <button>Cancel</button>
+                            </div>
+                                `;
+}
+
+// Delete tasks from todo list
