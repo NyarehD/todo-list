5b6294a91142aff0f8a5f85c41db98466e12bbd3
NyarehD <nyarehdavid56@gmail.com>
2021-04-11 10:05:01 +0630

    I don't know how to make edit function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let inputText = document.getElementById('inputTask');
+let inputTask = document.getElementById('inputTask');
 const addButton = document.getElementById('addButton');
 const todoList = document.getElementById('todoList');
 let inputDescription = document.getElementById('inputDescription');
@@ -14,28 +14,37 @@ addButton.addEventListener("click", ()=>{
 function inputTextToList(){
     todoList.innerHTML += `<li id="listId${listId}">
                                 <div>
-                                    <h3>${inputText.value}</h3>
-                                    <p>${inputDescription.value}</p>
+                                    <h3 id="taskId${listId}">${inputTask.value}</h3>
+                                    <p id="descriptionId${listId}">${inputDescription.value}</p>
                                 </div>
                                 <div>
                                     <button src="/assets/edit-regular.svg" onclick="editList('${listId}')">edit</button>
-                                    <button src="" onclick="deleteList('${listId})">Delete</button>
+                                    <button src="" onclick="deleteList(${listId})">Delete</button>
                                 </div>
                             </li>`;
     listId++;
 }
 // Edit tasks from todo list
 function editList(id){
-    let currentTask = document.getElementById(`listId${id}`);
-    currentTask.innerHTML = `<div>
-                                 <input placeholder="Edit your Task Name" value="$>
-                                 <textarea cols="30" row="5"></textarea>
+    const currentList = document.getElementById(`listId${id}`);
+    const currentTask = document.getElementById(`taskId${id}`);
+    const currentDescription = document.getElementById(`descriptionId${id}`);
+    currentList.innerHTML = `<div>
+                                 <input placeholder="Edit your Task Name" value="${currentTask.innerText}" id="taskId${id}">
+                                 <textarea cols="30" rows="5" id="descriptionId${id}">${currentDescription.innerText}</textarea>
                                 </div>   
                             <div>
-                                <button>Done</button>
+                                <button onclick="editList(${id}).done(${id})">Done</button>
                                 <button>Cancel</button>
                             </div>
                                 `;
+    function done(id) {
+        console.log("fick you");
+    }
 }
 
 // Delete tasks from todo list
+function deleteList(id){
+    let listItemToDelete = document.getElementById(`listId${id}`);
+    todoList.removeChild(listItemToDelete);
+}
